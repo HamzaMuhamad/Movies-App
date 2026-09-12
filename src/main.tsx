3a6858fe7b920da96f14b7f972d12fab10ae77ca
HamzaMuhamad <hamzamuhamd365@gmail.com>
2026-09-12 04:16:02 +0300

    publishing NavBar

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <section className="container">
+    <section className="container block">
       <Navbar />
     </section>
   </StrictMode>,
